abd10cc904041e33dfdb3c34907365dda90f8136
simret <simruineb@gmail.com>
2022-11-20 10:14:10 +0300

    Initial commit

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,10 +1,22 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
-import { TodoItem } from '../models/TodoItem'
+// import { TodosAccess } from './todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
+// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+// import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
-
-// TODO: Implement businessLogic
+// import * as createError from 'http-errors'
+import { getUserId } from '../lambda/utils';
+// import { APIGatewayProxyEvent } from 'aws-lambda';
+import { TodoItem } from '../models/TodoItem';
+// // TODO: Implement businessLogic
+export function todoBuilder(todoRequest: CreateTodoRequest, event): TodoItem { 
+    const todo = {
+      todoId: uuid.v4(),
+      userId: getUserId(event),
+      createdAt: new Date().toISOString(),
+      done: false,
+      attachmentUrl: '',
+      ...todoRequest
+       }
+       return todo
+}
--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
@@ -1,12 +1,47 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+const AWSXRay = require('aws-xray-sdk')
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-import { createLogger } from '../utils/logger'
+// import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
-import { TodoUpdate } from '../models/TodoUpdate';
+// import { TodoUpdate } from '../models/TodoUpdate';
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
-const logger = createLogger('TodosAccess')
+// const logger = createLogger('TodosAccess')
+const docClient: DocumentClient = createDynamoDBClient()
+const todosTable = process.env.TODOS_TABLE
 
 // TODO: Implement the dataLayer logic
+export async function createTodo(todo: TodoItem): Promise<TodoItem> {
+    await docClient
+      .put({
+        TableName: todosTable,
+        Item: todo
+      })
+      .promise()
+    return todo
+  }
+
+  export async function getAllTodosByUserId(userId: string): Promise<TodoItem[]> {
+    const result = await docClient
+      .query({
+        TableName: todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+          ':userId': userId
+        }
+      })
+      .promise();
+    return result.Items as TodoItem[]
+  }
+  function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+      console.log('Creating a local DynamoDB instance')
+      return new XAWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      })
+    }
+  
+    return new XAWS.DynamoDB.DocumentClient()
+}
