abc8b9a16da704ff9d84b92de628d0f1917599d5
simret <simruineb@gmail.com>
2022-11-21 18:03:52 +0300

    Create todo, get todo, get todos, upload

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
@@ -10,6 +10,7 @@ const XAWS = AWSXRay.captureAWS(AWS)
 // const logger = createLogger('TodosAccess')
 const docClient: DocumentClient = createDynamoDBClient()
 const todosTable = process.env.TODOS_TABLE
+const index = process.env.TODOS_CREATED_AT_INDEX
 
 // TODO: Implement the dataLayer logic
 export async function createTodo(todo: TodoItem): Promise<TodoItem> {
@@ -34,6 +35,41 @@ export async function createTodo(todo: TodoItem): Promise<TodoItem> {
       .promise();
     return result.Items as TodoItem[]
   }
+  
+  export async function getTodoById(todoId: string): Promise<TodoItem> {
+    const result = await docClient
+      .query({
+        TableName: todosTable,
+        IndexName: index,
+        KeyConditionExpression: 'todoId = :todoId',
+        ExpressionAttributeValues: {
+          ':todoId': todoId
+        }
+      })
+      .promise()
+      const items = result.Items
+      if (items.length !== 0)
+    return result.Items[0] as TodoItem
+    return null
+  }
+
+  export async function updateTodo(todo: TodoItem): Promise<TodoItem> {
+    const result = await docClient
+      .update({
+        TableName: todosTable,
+        Key: {
+          userId: todo.userId,
+          todoId: todo.todoId
+        },
+        UpdateExpression: 'set attachmentUrl = :attachmentUrl',
+        ExpressionAttributeValues: {
+          ':attachmentUrl': todo.attachmentUrl
+        }
+      })
+      .promise()
+    return result.Attributes as TodoItem
+  }
+
   function createDynamoDBClient() {
     if (process.env.IS_OFFLINE) {
       console.log('Creating a local DynamoDB instance')
