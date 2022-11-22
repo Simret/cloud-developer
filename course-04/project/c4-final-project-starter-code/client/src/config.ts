f6c3f326e47981117d830063e7a26b8ed46a0fe0
simret <simruineb@gmail.com>
2022-11-22 10:45:51 +0300

    config file updated

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'otscz3hkt4'
+const apiId = 'igf6pnyhfa'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
